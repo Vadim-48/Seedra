49645dbc4a4e67e0c6937daa0e21d1f6d2ed1db6
Vadim-48 <rvadim4835@gmail.com>
2026-03-17 14:30:15 +0200

    Updates

---
+++ assets/our-blog-BM2n_cdX.js
@@ -0,0 +1,1 @@
+import{b as e,a as o}from"./burger-accordion-fsrTvoOq.js";import{a as r}from"./firebase-D7ZZdk3h.js";console.log("Firebase ready",r);document.addEventListener("DOMContentLoaded",async()=>{e(),o()});

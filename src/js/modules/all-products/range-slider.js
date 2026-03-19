5e4930664084e7d4a7b11df897de9ad64072d691
Vadim-48 <rvadim4835@gmail.com>
2026-03-19 17:57:06 +0200

    19.03.26 fix range-slider for two el, done: header, footer, burger, pages: main, one-product, about-seedra, our-blog, card-news-page, contacts

--- src/js/modules/all-products/range-slider.js
+++ src/js/modules/all-products/range-slider.js
@@ -1,37 +1,41 @@
 import noUiSlider from 'nouislider';
 
 export const initRangeSlider = () => {
-    const rangeSlider = document.getElementById('range-slider');
-    if (rangeSlider) {
+    const sliders = document.querySelectorAll('[data-range-slider]');
+
+    sliders.forEach((rangeSlider) => {
+        const wrapper = rangeSlider.closest('.filter-form__price-wrap');
+        const priceMin = wrapper.querySelector('[data-price-min]');
+        const priceMax = wrapper.querySelector('[data-price-max]');
+        const prices = [priceMin, priceMax];
+
         noUiSlider.create(rangeSlider, {
-            start: [1, 1000],
+            start: [
+                Number(priceMin.value) || 1,
+                Number(priceMax.value) || 1000
+            ],
             connect: true,
             step: 1,
             range: {
-                'min': [1],
-                'max': [1000]
+                min: 1,
+                max: 1000
             }
         });
 
-        const priceMin = document.getElementById('price-min');
-        const priceMax = document.getElementById('price-max');
-        const prices = [priceMin, priceMax];
-
-        rangeSlider.noUiSlider.on('update', function (values, handle) {
+        rangeSlider.noUiSlider.on('update', (values, handle) => {
             prices[handle].value = Math.round(values[handle]);
         });
 
-        const setRangerSlider = (i, value) => {
+        const setRangeSlider = (i, value) => {
             let arr = [null, null];
-            arr[i] = value;
-
+            arr[i] = Number(value);
             rangeSlider.noUiSlider.set(arr);
         };
 
-        prices.forEach((price,index) => {
+        prices.forEach((price, index) => {
             price.addEventListener('change', (e) => {
-                setRangerSlider(index, e.currentTarget.value);
+                setRangeSlider(index, e.currentTarget.value);
             });
         });
-    }
+    });
 };

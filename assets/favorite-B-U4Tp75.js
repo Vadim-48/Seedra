7c55abc0e00257004ab88e4173a148496262f892
Vadim-48 <rvadim4835@gmail.com>
2026-03-29 07:54:03 +0300

    Updates

--- assets/favorite-B-U4Tp75.js
+++
@@ -1,1 +0,0 @@
-import{b as e,a as r}from"./burger-accordion-CF7kqAJH.js";document.addEventListener("DOMContentLoaded",async()=>{e(),r()});
---
+++ assets/favorite-DmsMbqaT.js
@@ -0,0 +1,1 @@
+import{b as e,a as r}from"./burger-accordion-CglSLtZ3.js";document.addEventListener("DOMContentLoaded",async()=>{e(),r()});

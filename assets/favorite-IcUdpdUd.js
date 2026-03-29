05865edf971dd292fe20613149bdb639c6c4d3f6
Vadim-48 <rvadim4835@gmail.com>
2026-03-29 07:49:15 +0300

    Updates

--- assets/favorite-IcUdpdUd.js
+++
@@ -1,1 +0,0 @@
-import{b as e,a as r}from"./burger-accordion-BaOIDHBM.js";document.addEventListener("DOMContentLoaded",async()=>{e(),r()});
--- assets/our-blog-DFy2TOKO.js
+++
@@ -1,1 +0,0 @@
-import{b as e,a as o}from"./burger-accordion-BaOIDHBM.js";import{a as r}from"./firebase-C67ONCVO.js";console.log("Firebase ready",r);document.addEventListener("DOMContentLoaded",async()=>{e(),o()});
